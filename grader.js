1954d18b68caba8d77c33b40e99b183d7f48b46d
Jair reina <jaireina@gmail.com>
2013-07-18 22:32:54 -0600

    I did it

--- grader.js
+++ grader.js
@@ -29,49 +29,58 @@ var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
-    var instr = infile.toString();
-    
-    if(fs.existsSync(instr)) {
-        return instr;
-        //console.url("file")
-        //console.log("%s does not exist. Exiting.", instr);
-        //process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }else if(instr.indexOf("http://") > -1 || instr.indexOf("https://") > -1){
-      rest.get(infile).on('complete', function(result) {
-        if (result instanceof Error) {
-          console.log("url");
-          console.log("%s does not exist. Exiting.", instr);
-          process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-        } else {
-          return instr;
-        }
-      });
-    }else{
-      console.log("url");
+  var instr = infile.toString();
+  if(!fs.existsSync(instr)) {
       console.log("%s does not exist. Exiting.", instr);
       process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
-
-    return instr;
+  }
+  return instr;
 };
+
+var assertUrlIsValid = function(url){
+  if(/((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(url)){
+    return url;
+  }else{
+    return false;
+  }
+
+};
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+var checkHtmlFile = function(htmlfile, checksfile, isFile, callback) {
+
+    var processCheck= function($){
+      var checks = loadChecks(checksfile).sort();
+      var out = {};
+      for(var ii in checks) {
+          var present = $(checks[ii]).length > 0;
+          out[checks[ii]] = present;
+      }
+      callback(out);
     }
-    return out;
+
+    if(isFile===true){
+      $ = cheerioHtmlFile(htmlfile);
+      processCheck($);  
+    }else{
+       rest.get( htmlfile ).on('complete', function( result ) {
+          if (result instanceof Error) {
+            console.log("%s does not exist. Exiting." + result.message, instr);
+            process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+          } else {
+            processCheck(cheerio.load(result));
+          }
+      });
+    }
 };
 
 var clone = function(fn) {
@@ -84,10 +93,22 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'URL to the file to check', clone(assertUrlIsValid), false)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var content = program.file;
+    var isFile = true;
+
+    if(program.url!=false){
+      content = program.url;
+      isFile = false;
+    }
+
+    checkHtmlFile(content, program.checks, isFile ,function(checkJson){
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    });
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
